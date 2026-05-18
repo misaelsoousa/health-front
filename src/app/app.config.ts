2f53f47f57c9bfb2b7ce599e4a7b6dbcf2a44733
misaelsoousa <misasoousa@gmail.com>
2026-05-18 15:37:49 -0300

    configure github deploy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
- import { API_BASE_URL } from './core/api/api-base-url';
+import { API_BASE_URL } from './core/api/api-base-url';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
@@ -10,7 +10,7 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withFetch()),
-    { provide: API_BASE_URL, useValue: 'http://localhost:8080/api/' },
-    provideClientHydration(withEventReplay())
-  ]
+    { provide: API_BASE_URL, useValue: 'http://ec2-52-15-82-33.us-east-2.compute.amazonaws.com:8080/api/' },
+    provideClientHydration(withEventReplay()),
+  ],
 };
